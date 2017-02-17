ca90f1726c4c124ffc0bf7c069ba5591aeef08ff
Adam Nyberg <adam.nyberg@gmail.com>
2017-02-17 13:35:22 +0100

    feat(A2): lab working, bad parameters

--- boosting/run.m
+++ boosting/run.m
@@ -2,6 +2,17 @@
 load faces, load nonfaces
 faces = double(faces);
 nonfaces = double(nonfaces);
+
+
+nbrHaarFeatures = 160;
+nbrTrainExamples = 400;
+nbrTestExamples = 3000;
+nbrClassifiers = 50;
+epsLim = 0.0001;
+defaultAlpha = 25;
+
+tic
+
 figure(1)
 colormap gray
 for k=1:25
@@ -14,7 +25,6 @@ for k=1:25
 end
 % Generate Haar feature masks
 
-nbrHaarFeatures = 25;
 haarFeatureMasks = GenerateHaarFeatureMasks(nbrHaarFeatures);
 figure(3)
 colormap gray
@@ -27,75 +37,123 @@ end
 
 % Create a training data set with a number of training data examples
 % from each class. Non-faces = class label y=-1, faces = class label y=1
-nbrTrainExamples = 100;
 
 trainImages = cat(3,faces(:,:,1:nbrTrainExamples),nonfaces(:,:,1:nbrTrainExamples));
-
 xTrain = ExtractHaarFeatures(trainImages,haarFeatureMasks);
 yTrain = [ones(1,nbrTrainExamples), -ones(1,nbrTrainExamples)];
 
+testImages = cat(3,faces(:,:,nbrTrainExamples:nbrTrainExamples+nbrTestExamples),nonfaces(:,:,nbrTrainExamples:nbrTrainExamples+nbrTestExamples));
+xTest = ExtractHaarFeatures(testImages,haarFeatureMasks);
+yTest = [ones(1,nbrTestExamples), -ones(1,nbrTestExamples)];
+
 
 N = nbrTrainExamples*2;
+Ntest = nbrTestExamples*2;
 M = nbrHaarFeatures;
-T = nbrHaarFeatures;
+T = nbrClassifiers;
 
-d = 1/N*ones(1, M);
-classifiers = zeros(5, T);
+d = 1/N*ones(1, N);
 
 for t = 1:T
     eps = zeros(M, N);
     pol = ones(M, N);
-    misClass = zeros(M, N)
-    
     
     for m = 1:M % features
         for n = 1:N % thresholds
             currentThreshold = xTrain(m, n);
             
-            xThreshold = +(xTrain(f,:) < currentThreshold);
+            xThreshold = +(xTrain(m,:) < currentThreshold);
             xThreshold(xThreshold == 0) = -1;
             I = +(xThreshold ~= yTrain);
             
             eps(m, n) = sum(d.*I);
             
-            if eps(m, n) < 0.5
+            if eps(m, n) > 0.5
                 eps(m, n) = 1 - eps(m, n);
                 pol(m, n) = -1;
-            end
-            
-            I(I == 0) = -1;
-            if p < 0
-                misClass(M, N) = ~I;
-            else
-                misClass(M, N) = I;
             end
         end
     end
     
     [feature threshold] = find(eps == min(eps(:))); %Finds all the best
     
-    feature = feature(1); 
+    feature = feature(1);
     threshold = threshold(1);
     epsilon = eps(feature, threshold);
     polarity = pol(feature, threshold);
-    misclassification = misClass(feature, threshold);
-    alpha = 0.5*log((1-epsilon)/epsilon);
     
-    classifiers(:,t) = [feature; threshold; epsilon; polarity; alpha]; % save best classifier
+    if epsilon > epsLim
+        alpha = 0.5*log((1-epsilon)/epsilon);
+    else
+        alpha = defaultAlpha;
+    end
     
-    d = d.*exp(-alpha * misClassification);
+    classifiers(:,t) = [epsilon; feature; threshold; polarity; alpha]; % save best classifier
     
-    d = max(0.1/(nbrTrainExamples*2), d); % Set max limit
-    d = min(10/(nbrTrainExamples*2), d); % Set min limit
+    xThreshold = +(xTrain(feature,:) < threshold);
+    xThreshold(xThreshold == 0) = -1;
+    I = +(xThreshold ~= yTrain);
+    
+    I(I == 0) = -1;
+    if pol(m, n) < 0
+        misClass = ~I;
+    else
+        misClass = I;
+    end
+    
+    d = d.*exp(-alpha * misClass);
+    
+    d = max(0.1/N, d); % Set max limit
+    d = min(10/N, d); % Set min limit
+    
     d = d/sum(d); % Normalize
 end
 
+correct = 0;
+h1 = 0;
 
-            
+for i = 1:Ntest
+    
+    h1 = strong(classifiers, xTest(:,i), T);
+    
+    if h1 == yTest(1,i)
+        correct = correct +1;   
+    end
+    
+end
+
+acc = correct/Ntest;
+
+
+trainingTime = toc;
+display(['Time spent training: ' num2str(trainingTime) ' sec'])
+display(['Accuracy: ' num2str(acc)])
             
     
         
         
+function h = strong(H, X, T)
+
+    h = 0;
+
+    for i = 1:T
+        % Weak classifiers features
+        epsilon = H(1,i);
+        feature = H(2,i);
+        threshold = H(3,i);
+        polarity = H(4,i);
+        alpha = H(5,i);
+        x = X(feature);
+
+        if x*polarity < threshold*polarity
+           h = h + alpha;
+        else
+           h = h - alpha;
+        end
+    end
+
+    h = sign(h);
+end
 
 
 
