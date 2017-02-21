ef05f222b2443f52e1a9aabb36f06668523e0862
Rasmus Johns <rasjo813@student.liu.se>
2017-02-21 09:12:43 +0100

    90%+

---
+++ boosting/strong.m
@@ -0,0 +1,25 @@
+function [h] = strong(H, X, T)
+
+    h = 0;
+
+    for i = 1:T
+        % Weak classifiers features
+        %epsilon = H(1,i);
+        feature = H(2,i);
+        threshold = H(3,i);
+        polarity = H(4,i);
+        alpha = H(5,i);
+        x = X(feature);
+
+        if x*polarity < threshold*polarity
+           h = h + alpha;
+        else
+           h = h - alpha;
+        end
+    end
+
+    h = sign(h);
+end
+
+
+
